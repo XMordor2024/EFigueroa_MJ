5972d92ed868e79c2e9683b47fc9039f619661bd
Ezequiel Figueroa <zequivid12@gmail.com>
2024-03-21 11:56:22 -0300

    update file

---
+++ labsc++/MO3Areacuadrado.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // variables 
+    int lado, area;
+    
+
+    cout<< "Ingrese el lado del cuadrado: ";
+    cin>> lado;
+
+    area = lado * lado;
+    
+    cout<< "El area del cuadrado es: "<<area<< endl;
+
+    return 0;
+}
