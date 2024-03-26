e4bc6d9b5a48f3ae3bf9916b7a6f7bef8e03dd60
Ezequiel Figueroa <zequivid12@gmail.com>
2024-03-26 08:58:57 -0400

    update file

---
+++ labsc++/Ejemplotrycatch.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numerador, denominador, resultado;
+    cout<< "Ingrese el numerador: ";
+    cin>>numerador;
+
+    cout<< "Ingrese el denominador: ";
+    cin>>denominador;
+
+    try
+    {
+        if(denominador==0)
+        {
+            throw"Division por cero no permitida.";//mensaje que describe el problema
+        }
+        resultado = numerador / denominador;
+
+        cout<< "Resultado:"<<resultado<<endl;
+    }
+    catch(const char* mensaje)
+    {
+        cout<<"Excepcion capturada:"<<mensaje<<endl;
+    }
+
+    return 0;
+    
+}
