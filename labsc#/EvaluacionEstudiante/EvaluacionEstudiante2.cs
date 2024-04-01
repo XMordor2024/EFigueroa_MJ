bd10f90cc8e1287c0b70f0dd01d6aeda2d041576
Ezequiel Figueroa <zequivid12@gmail.com>
2024-04-01 14:07:56 -0400

    update file

---
+++ labsc#/EvaluacionEstudiante/EvaluacionEstudiante2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace EvaluacionEstudiante2
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.Write("Ingrese la nota del estudiante (0-20): ");
+            double nota = double.Parse(Console.ReadLine());
+
+            if(nota >= 11)
+            {
+                Console.WriteLine("El estudiante aprobo la prueba.");
+            }
+            if(nota < 11)
+            {
+                Console.WriteLine("El estudiante no aprobo la prueba.");
+            }
+        }
+    }
+}
