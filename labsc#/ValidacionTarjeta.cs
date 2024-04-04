b7c4f39914e6356f7929fb1490f0237f6d61a54d
Ezequiel Figueroa <zequivid12@gmail.com>
2024-04-04 10:21:19 -0400

    update file

--- labsc#/ValidacionTarjeta.cs
+++ labsc#/ValidacionTarjeta.cs
@@ -3,16 +3,16 @@ using System;
 namespace ValidacionTarjeta{
     class Program{
         static void Main(string[]args){
-            Console.WriteLine("Ingrese un 1 para tarjeta activa o 0 para tarjeta inactiva: "); //Aquí se le pide al usuario que iingrse un valor para verificar la validacion de su tarjeta//
+            Console.WriteLine("Ingrese un 1 para tarjeta activa: "); //Aquí se le pide al usuario que iingrse un valor para verificar la validacion de su tarjeta//
             int tarjeta = int.Parse(Console.ReadLine());
 
             if(tarjeta == 1){
             Console.WriteLine("Su tarjeta es válida."); //En esta linea de codigo si el usuario ingresa el numero 1 un mensaje saldra diciendo que su tarjeta es valida.//
             }
 
-            if(tarjeta == 0){
-            Console.WriteLine("Su tarjeta no es válida.");
+            else{
+                Console.WriteLine("Su tarjeta no es valida.");
+        }
         }
     }
-    } 
 }
