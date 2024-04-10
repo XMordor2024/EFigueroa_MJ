2c6b32216a64256cc6f17f6cb2e545c7695aa672
Ezequiel Figueroa <zequivid12@gmail.com>
2024-04-10 11:19:48 -0400

    update file

--- labsc++/CalificacionEstudiante.cpp
+++ labsc++/CalificacionEstudiante.cpp
@@ -5,11 +5,19 @@ int main()
 {
     //clasificando variables a utilizar
     int sumaCalificaciones = 0;
-    int promedio, calificacionEstudiante;
+    int numeroEstudiante, promedio, calificacionEstudiante;
     string nombreEstudiante;
 
+    //Se pide la cantidad de estudiantes al usuario
+    cout <<"Ingrese la cantidad de estudiantes (no mas de 10) ";
+    cin >> numeroEstudiante;
+
+
+    if(numeroEstudiante <= 10) {
+        //cuenta la cantidad de estudiantes
+
     //cuenta los 10 estudiantes y las calificaciones de ellos
-    for(int i = 1; i <= 10; i++) {
+    for(int i = 1; i <= numeroEstudiante; i++) {
         
         //Se le pide al usuario que coloque el nombre del estudiante
         cout <<"Estudiante # "<< i << ": ";
@@ -17,12 +25,16 @@ int main()
         cin >> nombreEstudiante;
 
         //Se le pide al usuario la puntuacion del estudiante
-        cout <<"Ingrese la puntuacion de " <<nombreEstudiante <<": ";
+        cout <<"Ingrese la puntuacion de " <<numeroEstudiante <<": ";
         cin >> calificacionEstudiante;
 
         //Las calificaciones se acumulan
         sumaCalificaciones = sumaCalificaciones + calificacionEstudiante;
 
+     }
+    }
+    else {
+        cout <<"El numero de estudiante ingresado es mayor a 10"<<endl;
     }
     //El total de las calificaciones se divide por la cantidad de los estudiantes
     promedio = sumaCalificaciones/10;
