ad9ba4a505e2f073871e1e18d4cd9cdb0979efef
Ezequiel Figueroa <zequivid12@gmail.com>
2024-04-17 13:56:18 -0400

    update file

---
+++ TemperaturaPromedio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Temperaturas
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            //Se identifican las variables a utilizar
+            int i;
+            double promedio;
+            double temp = 0;
+            double sumaTemp = 0;
+
+            //En esta linea de codigo se comienza el ciclo de el ejercicio
+            do{
+
+                Console.WriteLine("Temperatura" + i);
+                Console.WriteLine("Ingrese la temperatura: ");
+                temp = double.Parse(Console.ReadLine());
+                i++;
+
+                sumaTemp = sumaTemp + temp;
+
+                promedio = sumaTemp/12;
+            }
+            while(i <= 12);
+
+            Console.WriteLine("La temperatura promedio es: "+promedio+"o");
+        }
+    }
+}
