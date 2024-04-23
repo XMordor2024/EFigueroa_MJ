72e6a4648a460891dddba0176a73af79f58f9866
Ezequiel Figueroa <zequivid12@gmail.com>
2024-04-23 12:38:57 -0400

    update file

---
+++ labsc++/Act.Funciones.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+//Funcion para calcuar el sueldo del vendedor
+double calcularSueldo(double ventas) {
+    double sueldoBase = ventas * 0.15;
+
+    if(ventas >= 1500) {
+        sueldoBase += 125;
+    }
+    return sueldoBase;
+}
+
+int main() {
+    double ventas;
+    cout <<"Ingrese el monto de ventas realizadas por el vendedor: ";//Se le pide al usuario que ingrese la cantidad de
+    cin >> ventas;                                                   //ventas del vendedor
+
+    //Llamada a la funcion para calcular el sueldo y muestra el resultado
+    double sueldo = calcularSueldo(ventas);
+    cout <<"El sueldo a pagar al vendedor es: $" << sueldo << endl;
+
+    return 0;
+}
