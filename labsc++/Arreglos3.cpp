b24d202d3f612445328da0201f216a9c895dcf05
Ezequiel Figueroa <zequivid12@gmail.com>
2024-04-30 09:46:56 -0400

    upate file

---
+++ labsc++/Arreglos3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    //Definir el arreglo con 5 nombres de ciudad o paises
+    string ciudades[5] = {"paris", "Tokio", "Nueva York", "Londres", "Sydney"};
+
+    //imprimir los nombres de las ciudades o paises
+    cout << "Lista de ciudades o paises: " <<endl;
+    for(int i = 0; i < 5; i++)
+    {
+        cout <<"Ciudad/Pais "<< i + 1 << ": " << ciudades[i] << endl;
+    }
+
+    return 0;
+}
